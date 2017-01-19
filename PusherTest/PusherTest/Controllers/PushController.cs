0fd02074e0ed2a6216fb6b414100a585d2ba6551
Joe Banken <jbanken@liftbrands.com>
2017-01-19 08:09:54 -0600

    Initial Commit

---
+++ PusherTest/PusherTest/Controllers/PushController.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Mvc;
+using PusherServer;
+using PusherTest.Models;
+
+namespace PusherTest.Controllers
+{
+    public class PushController : Controller
+    {
+        [ValidateInput(false)]
+        public ContentResult Send([FromBody]PushSend request)
+        {
+            var options = new PusherOptions();
+            options.Cluster = "eu";
+            options.Encrypted = true;
+            var pusher = new Pusher("291363", "48021ed1b96971d522e7", "663be45a23c99fdc4dd0", options);
+
+            var result = pusher.Trigger(request.Channel, "Event:Listen", new { message = request.Message });
+
+            return Content(result.ToString(), "application/json");
+        }
+
+    }
+}
---
+++ PusherTest/PusherTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PusherTest.MvcApplication" Language="C#" %>
